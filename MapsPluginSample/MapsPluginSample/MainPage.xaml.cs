7423bf7f721d7c7400402ee34cca71354b1d53b6
androidmads <mushtaqat3gb@gmail.com>
2018-10-14 22:51:17 +0530

    Maps Plugin Sample

---
+++ MapsPluginSample/MapsPluginSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.MapsPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MapsPluginSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            CrossMapsPlugin.Current.PinTo("Vannarapettai", 10.7653, 79.0687, 8);
+        }
+    }
+}
